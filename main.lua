a453e1f53304e36ac056163091b1b2e2f3555422
pyxledev0 <pyxledev0@gmail.com>
2022-07-24 16:19:29 +0300

    paddle drawing

--- main.lua
+++ main.lua
@@ -10,12 +10,15 @@ function love.load()
     Paddle2 = paddle.new()
     Paddle2.id = 2
     Paddle2.position = vec2.new(940, 270)
+    PaddleImg = love.graphics.newImage("images/paddle.png")
 end
 
-function love.update(_elta)
-    
+function love.update(delta)
+    Paddle1.update(delta)
+    Paddle2.update(delta)
 end
 
 function love.draw()
-    
+    Paddle1.draw()
+    Paddle2.draw()
 end
--- scripts/paddle.lua
+++ scripts/paddle.lua
@@ -8,6 +8,19 @@ function paddle.new()
         id = 1;
     }
 
+    function p.update(delta)
+        
+    end
+
+    function p.draw()
+        local width = PaddleImg:getWidth()
+        local height = PaddleImg:getHeight()
+        love.graphics.draw(
+            PaddleImg, p.position.x, p.position.y, 0,
+            1, 1, width/2, height/2
+        )
+    end
+
     return p
 end
 
