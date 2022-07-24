d6c1917e27c8dde3d1358c8c659d76f5e911008d
pyxledev0 <pyxledev0@gmail.com>
2022-07-24 16:54:26 +0300

    ball edge bounce mechanic

--- scripts/ball.lua
+++ scripts/ball.lua
@@ -5,18 +5,30 @@ local ball = {}
 function ball.new()
     local b = {
         position = vec2.new(480, 270);
-        rotation = math.random();
+        xSpeed = 1; ySpeed = 1;
         img = love.graphics.newImage("images/ball.png");
     }
 
+    function b.bounce(delta)
+        local width = b.img:getWidth()
+        local height = b.img:getHeight()
+        if b.position.x < width/2 or b.position.x > 960 - width/2 then
+            b.xSpeed = -b.xSpeed
+        end
+        if b.position.y < height/2 or b.position.y > 540 - height/2 then
+            b.ySpeed = -b.ySpeed
+        end
+    end
+
     function b.movement(delta)
         local speed = 100
-        b.position.x = b.position.x + math.cos(b.rotation) * speed * delta
-        b.position.y = b.position.y + math.sin(b.rotation) * speed * delta
+        b.position.x = b.position.x + b.xSpeed * speed * delta
+        b.position.y = b.position.y + b.ySpeed * speed * delta
     end
 
     function b.update(delta)
         b.movement(delta)
+        b.bounce(delta)
     end
 
     function b.draw()
