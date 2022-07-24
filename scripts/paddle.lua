aa824e15b6e344a9676f4bf39427958c1be8cb96
pyxledev0 <pyxledev0@gmail.com>
2022-07-24 16:24:47 +0300

    paddle movement

--- scripts/paddle.lua
+++ scripts/paddle.lua
@@ -8,8 +8,35 @@ function paddle.new()
         id = 1;
     }
 
+    function p.movement(delta)
+        -- Determine input keys based on ID
+        local inpKey
+        if p.id == 1 then
+            inpKey = {"w", "s"} else
+            inpKey = {"up", "down"} end
+        -- Move paddle
+        local speed = 145
+        if love.keyboard.isDown(inpKey[1]) then
+            p.position.y = p.position.y - speed * delta
+        end
+        if love.keyboard.isDown(inpKey[2]) then
+            p.position.y = p.position.y + speed * delta
+        end
+        -- Make sure paddle stays on screen
+        p.clip()
+    end
+
+    function p.clip()
+        local height = PaddleImg:getHeight()
+        if p.position.y < height/2 + 5 then
+            p.position.y = height/2 + 5
+        elseif p.position.y > 535 - height/2 then
+            p.position.y = 535 - height/2
+        end
+    end
+
     function p.update(delta)
-        
+        p.movement(delta)
     end
 
     function p.draw()
